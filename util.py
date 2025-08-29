15a3f648c97ef71d899203246e82957086ca8561
rifani012 <citizen.z027@gmail.com>
2025-08-29 12:26:52 +0800

    Add files via upload

---
+++ util.py
@@ -0,0 +1,38 @@
+import os, json
+import sys
+
+from api_request import *
+from ui import *
+
+def load_token(api_key: str):
+    if os.path.exists("tokens.json"):
+        with open("tokens.json", "r", encoding="utf8") as f:
+            tokens = json.load(f)
+        print("Tokens loaded successfully.")
+        
+        refresh_token = tokens.get("refresh_token")
+        tokens = get_new_token(refresh_token)
+        
+        id_token = tokens.get("id_token")
+        access_token = tokens.get("access_token")
+        
+        profile = get_profile(api_key, access_token, id_token)
+        if not profile:
+            print("Failed to fetch profile. Please check your tokens.")
+            sys.exit(1)
+        
+        phone_number = profile.get("profile").get("msisdn")
+        
+        balance = get_balance(api_key, id_token)
+        balance_remaining = balance.get("remaining")
+        balance_expired_at = balance.get("expired_at")
+        
+        return {
+            "tokens": tokens,
+            "is_logged_in": True,
+            "phone_number": phone_number,
+            "balance": balance_remaining,
+            "balance_expired_at": balance_expired_at,
+        }
+    
+    return None
