d833c1919ce89bcb42329e869c094a2520ce7c39
rifani012 <citizen.z027@gmail.com>
2025-08-29 18:48:19 +0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
 import sys
 
-from api_request import *
+from api_request2 import *
 from ui import *
 from util2 import load_token
 from paket_xut import get_package_xut
@@ -20,7 +20,7 @@ api_key = ""
 show_menu = True
 def main():
     global api_key
-    api_key = "e8e1e10a-d19f-4787-b04b-ad1a857835b9"
+    api_key = "b591791b-34ee-4a19-907b-60fe58e4c122"
     
     while True:
         updated_user_data = load_token(api_key)
